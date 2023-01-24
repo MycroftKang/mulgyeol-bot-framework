a63ebb1791558c04efff363b38012c290ae06bc5
Mycroft Kang <thkang0629@gmail.com>
2023-01-24 20:19:29 +0900

    Add pyproject.toml

--- MGBotBuilder/__init__.py
+++ MGBotBuilder/__init__.py
@@ -1,3 +1,5 @@
 from MGBotBuilder.Console import CommandConsole
 from MGBotBuilder.Console import VirtualCommandConsole
 from MGBotBuilder.utils import Request
+
+__version__ = "0.0.1a"
--- setup.py
+++ setup.py
@@ -1,8 +1,42 @@
 import setuptools
+import re
+
+version = ""
+with open("MGBotBuilder/__init__.py") as f:
+    version = re.search(
+        r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', f.read(), re.MULTILINE
+    ).group(1)
+
+if not version:
+    raise RuntimeError("version is not set")
+
+if version.endswith(("a", "b", "rc")):
+    # append version identifier based on commit count
+    try:
+        import subprocess
+
+        p = subprocess.Popen(
+            ["git", "rev-list", "--count", "HEAD"],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+        )
+        out, err = p.communicate()
+        if out:
+            version += out.decode("utf-8").strip()
+        p = subprocess.Popen(
+            ["git", "rev-parse", "--short", "HEAD"],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+        )
+        out, err = p.communicate()
+        if out:
+            version += "+g" + out.decode("utf-8").strip()
+    except Exception:
+        pass
 
 setuptools.setup(
     name="mulgyeol-bot-framework",
-    version="0.0.1",
+    version=version,
     author="Mycroft Kang",
     author_email="thkang0629@gmail.com",
     description="Mulgyeol Bot Framework",
